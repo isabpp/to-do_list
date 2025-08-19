36ae27d529324cf158426623fb62d4494cf369a1
Isabella <96348348+isabpp@users.noreply.github.com>
2025-08-19 15:00:14 +0300

    Created Task class

---
+++ src/main/java/models/Task.java
@@ -0,0 +1,18 @@
+package models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+//this class will be translated into a table in the database later
+@Entity
+@Data
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private boolean completed;
+}
