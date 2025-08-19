f1b6f17ae13dff7046d638b9d2f7dc7443a1bbfa
Isabella <96348348+isabpp@users.noreply.github.com>
2025-08-19 16:16:18 +0300

    Added TaskController and TaskService

---
+++ src/main/java/com/todo/todo_list/services/TaskService.java
@@ -0,0 +1,12 @@
+package com.todo.todo_list.services;
+
+import com.todo.todo_list.models.Task;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    public List<Task> getAllTasks() {
+    }
+}
