86c2ca1d6bd2131e5c02d96a6450695727e74477
Isabella <96348348+isabpp@users.noreply.github.com>
2025-08-19 16:24:18 +0300

    Updated TaskService

--- src/main/java/com/todo/todo_list/services/TaskService.java
+++ src/main/java/com/todo/todo_list/services/TaskService.java
@@ -1,12 +1,21 @@
 package com.todo.todo_list.services;
 
 import com.todo.todo_list.models.Task;
+import com.todo.todo_list.repository.TaskRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
     public List<Task> getAllTasks() {
+        return taskRepository.findAll();
     }
 }
