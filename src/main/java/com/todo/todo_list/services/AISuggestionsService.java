419523cfc4e4c08b7b0269f30854227d9d5b76a5
Isabella <96348348+isabpp@users.noreply.github.com>
2026-03-08 18:59:42 +0200

    Added generateSuggestion method

--- src/main/java/com/todo/todo_list/services/AISuggestionsService.java
+++ src/main/java/com/todo/todo_list/services/AISuggestionsService.java
@@ -2,12 +2,24 @@ package com.todo.todo_list.services;
 
 import com.google.genai.Client;
 import com.google.genai.types.GenerateContentResponse;
+import com.todo.todo_list.repository.TaskRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AISuggestionsService {
     private final Client client;
 
-    public AISuggestionsService(Client client) {
-        String apiKey = System.getenv("GEM_API_KEY");
+    public AISuggestionsService() {
+        String apiKey = System.getenv("GEMINI_API_KEY");
         this.client = Client.builder().apiKey(apiKey).build();
+    }
+
+    public String generateSuggestion(String task) {
+        GenerateContentResponse response = client.models.generateContent(
+                "gemini-3-flash-preview",
+                "Provide a suggestion for: " + task,
+                null
+        );
+        return response.text();
     }
 }
